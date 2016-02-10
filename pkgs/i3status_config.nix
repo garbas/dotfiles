020cc72b387f00b7347bd73bd53172fd2742571a
Rok Garbas <rok@garbas.si>
2016-02-10 13:55:42 +0100

    i3status_config: fix showing of the battery_level

--- pkgs/i3status_config.nix
+++ pkgs/i3status_config.nix
@@ -15,35 +15,35 @@ order += "ethernet eth0"
 order += "cpu_temperature 0"
 order += "sysdata"
 order += "time"
-order += "battery_level all"
+order += "battery_level"
 order += "xrandr"
 
 battery_level {
   notify_low_level = true
-  format = "{percent} {icon}"
+  format = "{percent}% {icon}"
 }
 
 wireless wlp3s0 {
-        format_up = "W: (%quality at %essid) %ip"
-        format_down = "W: down"
+  format_up = "W: (%quality at %essid) %ip"
+  format_down = "W: down"
 }
 
 ethernet eth0 {
-        # if you use %speed, i3status requires root privileges
-        format_up = "E: %ip (%speed)"
-	format_down = "E: down"
+  # if you use %speed, i3status requires root privileges
+  format_up = "E: %ip (%speed)"
+  format_down = "E: down"
 }
 
 disk "/" {
-	format = "%free"
+  format = "%free"
 }
 
 cpu_temperature 0 {
-	format = "T: %degrees °C"
+  format = "T: %degrees °C"
 }
 
 time {
-	format = "%Y-%m-%d %H:%M:%S"
+  format = "%Y-%m-%d %H:%M:%S"
 }
 
 ''
