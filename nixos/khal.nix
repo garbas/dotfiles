56a0096506e7198469befe81a86388545a2fe017
Rok Garbas <rok@garbas.si>
2020-03-09 09:11:19 +0100

    khal: remove some comments

--- nixos/khal.nix
+++ nixos/khal.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, lib, ... }:
 
 let
@@ -57,11 +53,9 @@ in {
 
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
 
-  # High-DPI console
   console.keyMap = "us";
   console.font = lib.mkDefault "${pkgs.terminus_font}/share/consolefonts/ter-u28n.psf.gz";
 
-  # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
@@ -223,9 +217,6 @@ in {
   services.xserver.libinput.enable = true;
   services.xserver.libinput.disableWhileTyping = true;
   services.xserver.libinput.naturalScrolling = false;
-  #services.xserver.libinput.additionalOptions = ''
-  #  Option "PalmDetection" "True"
-  #'';
 
   services.xserver.desktopManager.xterm.enable = false;
 
@@ -257,7 +248,6 @@ in {
   security.sudo.wheelNeedsPassword = false;
   security.hideProcessInformation = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.defaultUserShell = pkgs.zsh;
   users.mutableUsers = false;
   users.users."rok" = {
@@ -270,12 +260,6 @@ in {
     home = "/home/rok";
   };
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "20.09"; # Did you read the comment?
 
   fonts.enableFontDir = true;
