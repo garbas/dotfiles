a87f51f8172ca6578cf0719d31be915a9f0f0a01
Rok Garbas <rok@garbas.si>
2020-08-19 23:57:33 +0200

    khal: update

--- nixos/khal.nix
+++ nixos/khal.nix
@@ -5,6 +5,36 @@ let
   khal-overlay = self: super: {
     neovim = import ./../../nvim-config { pkgs = super; };
     dunst = super.dunst.override { dunstify = true; };
+    uhk-agent =
+      let
+        version = "1.2.12";
+        #version = "1.4.3";
+        image = self.stdenv.mkDerivation {
+          name = "uhk-agent-image";
+          src = self.fetchurl {
+            url = "https://github.com/UltimateHackingKeyboard/agent/releases/download/v${version}/UHK.Agent-${version}-linux-x86_64.AppImage";
+            sha256 = "1gr3q37ldixcqbwpxchhldlfjf7wcygxvnv6ff9nl7l8gxm732l6";
+            #sha256 = "sha256-zbQyouXC8qebYeYbyyl7fulYe4rRens5j+LvW7y8bqI=";
+          };
+          buildCommand = ''
+            mkdir -p $out
+            cp $src $out/appimage
+            chmod ugo+rx $out/appimage
+          '';
+        };
+      in self.runCommand "uhk-agent" {} ''
+        mkdir -p $out/bin $out/etc/udev/rules.d 
+        echo "${self.appimage-run}/bin/appimage-run ${image}/appimage" > $out/bin/uhk-agent
+        chmod +x $out/bin/uhk-agent
+        cat > $out/etc/udev/rules.d/50-uhk60.rules <<EOF
+        # Ultimate Hacking Keyboard rules
+        # These are the udev rules for accessing the USB interfaces of the UHK as non-root users.
+        # Copy this file to /etc/udev/rules.d and physically reconnect the UHK afterwards.
+        SUBSYSTEM=="input", GROUP="input", MODE="0666"
+        SUBSYSTEMS=="usb", ATTRS{idVendor}=="1d50", ATTRS{idProduct}=="612[0-7]", MODE:="0666", GROUP="plugdev"
+        KERNEL=="hidraw*", ATTRS{idVendor}=="1d50", ATTRS{idProduct}=="612[0-7]", MODE="0666", GROUP="plugdev"
+        EOF
+      '';
   };
 in {
   imports =
@@ -41,8 +71,22 @@ in {
   nix.maxJobs = lib.mkDefault 8;
   nix.package = pkgs.nixFlakes;
   nix.useSandbox = true;
+  nix.trustedUsers = ["@wheel"];
+  nix.distributedBuilds = true;
+  nix.buildMachines = [
+      # tweag remote builder
+      {
+        hostName = "build01.tweag.io";
+        maxJobs = 24;
+        sshUser = "nix";
+        sshKey = "/root/.ssh/id-tweag-builder";
+        system = "x86_64-linux";
+        supportedFeatures = [ "benchmark" "big-parallel" "kvm" ];
+      }
+    ];
   nix.extraOptions = ''
     experimental-features = nix-command flakes
+    builders-use-substitutes = true
   '';
 
   nixpkgs.config.allowBroken = false;
@@ -82,11 +126,14 @@ in {
 
   time.timeZone = "Europe/Ljubljana";
 
-  environment.variables = {
-    GDK_SCALE = "2";
-    GDK_DPI_SCALE = "0.5";
-    QT_AUTO_SCREEN_SCALE_FACTOR = "1";
-  };
+  environment.variables.GDK_SCALE = "2";
+  environment.variables.GDK_DPI_SCALE = "0.5";
+  environment.variables.QT_AUTO_SCREEN_SCALE_FACTOR = "1";
+  environment.variables.NIX_PATH = lib.mkForce "nixpkgs=/etc/nixos/nixpkgs-channels:nixos-config=/etc/nixos/configuration.nix";
+  environment.variables.GIT_EDITOR = lib.mkForce "nvim";
+  environment.variables.EDITOR = lib.mkForce "nvim";
+  environment.variables.FZF_DEFAULT_COMMAND = "rg --files";
+
   environment.systemPackages = with pkgs; [
 
     # email
@@ -98,6 +145,10 @@ in {
     w3m
     imapnotify
     msmtp
+    khal
+    khard
+    vdirsyncer
+    noti
 
     # editors
     neovim
@@ -106,15 +157,17 @@ in {
     # chat
     skype
     zoom-us
-    riot-desktop
+    element-desktop
+    discord
 
     # terminals
     alacritty
     termite
-    hyper
 
     # nix tools
+    nixpkgs-fmt
     niv
+    direnv
 
     # browsers
     firefox
@@ -124,17 +177,18 @@ in {
     # version control
     gitAndTools.gitflow
     gitAndTools.hub
+    gitAndTools.gh
     gitFull
+    git-town
+    git-lfs
     mercurialFull
 
     # i3 related
-    gnome3.adwaita-icon-theme
-    gnome3.dconf
-    gnome3.defaultIconTheme
-    hicolor-icon-theme
     rofi
-    # rofi-menugen  # TODO: do i need this?
-    # rofi-calc  # TODO: https://github.com/NixOS/nixpkgs/pull/83136
+    rofi-calc
+    rofi-emoji
+    rofi-file-browser
+    rofi-systemd
     volumeicon
     xclip
 
@@ -145,17 +199,24 @@ in {
     i3status-rust
 
     # gui tools
+    dropbox
+    keybase-gui
     obs-studio
     pavucontrol
+    transmission-gtk
+    uhk-agent
     zathura
-    keybase-gui
 
     # console tools
     asciinema
+    awscli
+    bat
     docker_compose
     entr
+    feh
     file
     fzf
+    gopass
     htop
     lastpass-cli
     mpv
@@ -175,20 +236,22 @@ in {
 
   documentation.info.enable = true;
 
-  programs.ssh.forwardX11 = false;
-  programs.mtr.enable = true;
+  programs.dconf.enable = true;
   programs.gnupg.agent.enable = true;
-  programs.gnupg.agent.enableSSHSupport = true;
   programs.gnupg.agent.enableBrowserSocket = true;
+  programs.gnupg.agent.enableSSHSupport = true;
   programs.mosh.enable = true;
-  programs.zsh.enable = true;
+  programs.mtr.enable = true;
+  programs.ssh.forwardX11 = false;
   programs.zsh.autosuggestions.enable = true;
+  programs.zsh.enable = true;
   programs.zsh.enableCompletion = true;
   programs.zsh.syntaxHighlighting.enable = true;
   programs.zsh.shellInit = ''
     bindkey "^[[A" history-substring-search-up
     bindkey "^[[B" history-substring-search-down
     eval "$(starship init zsh)"
+    eval "$(direnv hook zsh)"
   '';
   programs.zsh.ohMyZsh.enable = true;
   programs.zsh.ohMyZsh.plugins =
@@ -212,9 +275,10 @@ in {
   services.keybase.enable = true;
   services.locate.enable = true;
   services.openssh.enable = true;
-  services.printing.drivers = with pkgs; [ hplip ];
+  services.printing.drivers = with pkgs; [ ]; # XXX: hplip ];
   services.printing.enable = true;
   services.timesyncd.enable = true;
+  services.udev.packages = with pkgs; [ uhk-agent ];
   services.zfs.autoScrub.enable = true;
   services.zfs.autoSnapshot.enable = true;
 
@@ -231,8 +295,11 @@ in {
   services.xserver.libinput.naturalScrolling = false;
 
   services.xserver.desktopManager.xterm.enable = false;
+  services.xserver.desktopManager.xfce.enable = true;
+  services.xserver.desktopManager.xfce.noDesktop = true;
+  services.xserver.desktopManager.xfce.enableXfwm = false;
 
-  services.xserver.displayManager.defaultSession = "none+i3";
+  services.xserver.displayManager.defaultSession = "xfce+i3";
   services.xserver.displayManager.sessionCommands = ''
     ${pkgs.xorg.xrdb}/bin/xrdb -merge <<EOF
       Xcursor.theme: Adwaita
@@ -240,21 +307,75 @@ in {
     EOF
     
   '';
+  services.xserver.displayManager.autoLogin.user = "rok";
+  services.xserver.displayManager.autoLogin.enable = true;
   services.xserver.displayManager.lightdm.enable = true;
   services.xserver.displayManager.lightdm.greeter.enable = false;
-  services.xserver.displayManager.lightdm.autoLogin.enable = true;
-  services.xserver.displayManager.lightdm.autoLogin.user = "rok";
 
   services.xserver.windowManager.i3.enable = true;
 
   virtualisation.docker.enable = true;
-  virtualisation.virtualbox.host.enable = true;
+  # TODO:
+  # make[4]: *** [/nix/store/7j21r60aa84gan4l9xfhsj08m1vxvbqi-linux-5.6.4-dev/lib/modules/5.6.4/source/scripts/Makefile.build:268: /build/virtualbox-6.1.4-modsrc/vboxdrv/r0drv/linux/memobj-r0drv-linux.o] Error 1
+  # make[4]: *** Waiting for unfinished jobs....
+  # In file included from /build/virtualbox-6.1.4-modsrc/vboxdrv/r0drv/linux/waitqueue-r0drv-linux.h:38,
+  #                  from /build/virtualbox-6.1.4-modsrc/vboxdrv/r0drv/linux/semevent-r0drv-linux.c:42:
+  # /build/virtualbox-6.1.4-modsrc/vboxdrv/include/iprt/time.h: In function 'RTTimeSpecGetTimeval':
+  # /build/virtualbox-6.1.4-modsrc/vboxdrv/include/iprt/time.h:379:13: error: dereferencing pointer to incomplete type 'struct timeval'
+  #   379 |     pTimeval->tv_sec = (time_t)i64;
+  #       |             ^~
+  # /build/virtualbox-6.1.4-modsrc/vboxdrv/include/iprt/time.h:379:25: error: 'time_t' undeclared (first use in this function); did you mean 'ktime_t'?
+  #   379 |     pTimeval->tv_sec = (time_t)i64;
+  #       |                         ^~~~~~
+  #       |                         ktime_t
+  # /build/virtualbox-6.1.4-modsrc/vboxdrv/include/iprt/time.h:379:25: note: each undeclared identifier is reported only once for each function it appears in
+  # /build/virtualbox-6.1.4-modsrc/vboxdrv/include/iprt/time.h:379:32: error: expected ';' before 'i64'
+  #   379 |     pTimeval->tv_sec = (time_t)i64;
+  #       |                                ^~~
+  #       |                                ;
+  # /build/virtualbox-6.1.4-modsrc/vboxdrv/include/iprt/time.h: In function 'RTTimeSpecSetTimeval':
+  # /build/virtualbox-6.1.4-modsrc/vboxdrv/include/iprt/time.h:393:67: error: dereferencing pointer to incomplete type 'const struct timeval'
+  #   393 |     return RTTimeSpecAddMicro(RTTimeSpecSetSeconds(pTime, pTimeval->tv_sec), pTimeval->tv_usec);
+  #       |                                                                   ^~
+  # make[4]: *** [/nix/store/7j21r60aa84gan4l9xfhsj08m1vxvbqi-linux-5.6.4-dev/lib/modules/5.6.4/source/scripts/Makefile.build:267: /build/virtualbox-6.1.4-modsrc/vboxdrv/r0drv/linux/semevent-r0drv-linux.o] Error 1
+  # In file included from /build/virtualbox-6.1.4-modsrc/vboxdrv/r0drv/linux/waitqueue-r0drv-linux.h:38,
+  #                  from /build/virtualbox-6.1.4-modsrc/vboxdrv/r0drv/linux/semeventmulti-r0drv-linux.c:42:
+  # /build/virtualbox-6.1.4-modsrc/vboxdrv/include/iprt/time.h: In function 'RTTimeSpecGetTimeval':
+  # /build/virtualbox-6.1.4-modsrc/vboxdrv/include/iprt/time.h:379:13: error: dereferencing pointer to incomplete type 'struct timeval'
+  #   379 |     pTimeval->tv_sec = (time_t)i64;
+  #       |             ^~
+  # /build/virtualbox-6.1.4-modsrc/vboxdrv/include/iprt/time.h:379:25: error: 'time_t' undeclared (first use in this function); did you mean 'ktime_t'?
+  #   379 |     pTimeval->tv_sec = (time_t)i64;
+  #       |                         ^~~~~~
+  #       |                         ktime_t
+  # /build/virtualbox-6.1.4-modsrc/vboxdrv/include/iprt/time.h:379:25: note: each undeclared identifier is reported only once for each function it appears in
+  # /build/virtualbox-6.1.4-modsrc/vboxdrv/include/iprt/time.h:379:32: error: expected ';' before 'i64'
+  #   379 |     pTimeval->tv_sec = (time_t)i64;
+  #       |                                ^~~
+  #       |                                ;
+  # /build/virtualbox-6.1.4-modsrc/vboxdrv/include/iprt/time.h: In function 'RTTimeSpecSetTimeval':
+  # /build/virtualbox-6.1.4-modsrc/vboxdrv/include/iprt/time.h:393:67: error: dereferencing pointer to incomplete type 'const struct timeval'
+  #   393 |     return RTTimeSpecAddMicro(RTTimeSpecSetSeconds(pTime, pTimeval->tv_sec), pTimeval->tv_usec);
+  #       |                                                                   ^~
+  # make[4]: *** [/nix/store/7j21r60aa84gan4l9xfhsj08m1vxvbqi-linux-5.6.4-dev/lib/modules/5.6.4/source/scripts/Makefile.build:267: /build/virtualbox-6.1.4-modsrc/vboxdrv/r0drv/linux/semeventmulti-r0drv-linux.o] Error 1
+  # make[3]: *** [/nix/store/7j21r60aa84gan4l9xfhsj08m1vxvbqi-linux-5.6.4-dev/lib/modules/5.6.4/source/Makefile:1683: /build/virtualbox-6.1.4-modsrc/vboxdrv] Error 2
+  # make[2]: *** [/nix/store/7j21r60aa84gan4l9xfhsj08m1vxvbqi-linux-5.6.4-dev/lib/modules/5.6.4/source/Makefile:180: sub-make] Error 2
+  # make[2]: Leaving directory '/nix/store/7j21r60aa84gan4l9xfhsj08m1vxvbqi-linux-5.6.4-dev/lib/modules/5.6.4/build'
+  # make[1]: *** [/build/virtualbox-6.1.4-modsrc/vboxdrv/Makefile-footer.gmk:114: vboxdrv] Error 2
+  # make[1]: Leaving directory '/build/virtualbox-6.1.4-modsrc/vboxdrv'
+  # make: *** [Makefile:58: vboxdrv] Error 2
+  # builder for '/nix/store/0yflxycd2kc7x28a0jjh788cjab97n6k-virtualbox-modules-6.1.4-5.6.4.drv' failed with exit code 2
+  # cannot build derivation '/nix/store/w6phy8ydgbzml11yb4xy2f1s9xahnicg-kernel-modules.drv': 1 dependencies couldn't be built
+  # cannot build derivation '/nix/store/588dzq7cv60xa5f4lh1bnwqx90kc0aab-linux-5.6.4-modules.drv': 1 dependencies couldn't be built
+  # building '/nix/store/fxssfq5la12v6wb3zcw4ydd8nrk68vlf-vscode-1.44.1.drv'...
+  # cannot build derivation '/nix/store/i5a1dmrkzv0qb2q27g0wpn4zrrcygv9k-nixos-system-khal-20.09.git.b3c3a0bd183.drv': 1 dependencies couldn't be built
+  # error: build of '/nix/store/i5a1dmrkzv0qb2q27g0wpn4zrrcygv9k-nixos-system-khal-20.09.git.b3c3a0bd183.drv' failed
+  #virtualisation.virtualbox.host.enable = true;
 
   sound.enable = true;
   hardware.pulseaudio.enable = true;
   hardware.opengl.driSupport = true;
   hardware.opengl.driSupport32Bit = true;
-  hardware.u2f.enable = true;
 
   security.sudo.enable = true;
   security.sudo.wheelNeedsPassword = false;
@@ -262,6 +383,9 @@ in {
 
   users.defaultUserShell = pkgs.zsh;
   users.mutableUsers = false;
+  users.users."root" = {
+    hashedPassword = "$6$sBFfflUBZtZMD$h.EWNsmmX8iwTM7jShIvYwvS2/h7dncGTNhG.yPN1dOte1Et0TTz7HSFmzkuWjQpnBAfANYdptF3EQoUNSYwx/";
+  };
   users.users."rok" = {
     hashedPassword = "$6$PS.1SD6/$kUv8wdXYH00dEvpqlC9SyX/E3Zm3HLPNmsxLwteJSQgpXDOfFZhWXkHby6hvZ.kFN2JbgXqJvwZfjOunBpcHX0";
     isNormalUser = true;
