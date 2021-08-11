9ad9cd53d366615c0ffc9963c15c75fd4887578b
Rok Garbas <rok@garbas.si>
2021-08-11 10:20:57 +0200

    removing custom uhk agent code in khal.nix

--- configurations/khal.nix
+++ configurations/khal.nix
@@ -9,7 +9,6 @@ let
   nixpkgs-mozilla-overlay = self: super: {};
 
   custom-overlay-old = self: super: {
-
     neofetch = super.neofetch.overrideAttrs (old: {
       patches = (self.lib.optionals (builtins.hasAttr "patches" old) old.patches) ++ [
         (self.fetchurl { 
@@ -19,37 +18,6 @@ let
         })
       ];
     });
-
-    uhk-agent =
-      let
-        version = "1.2.12";
-        #version = "1.4.3";
-        image = self.stdenv.mkDerivation {
-          name = "uhk-agent-image";
-          src = self.fetchurl {
-            url = "https://github.com/UltimateHackingKeyboard/agent/releases/download/v${version}/UHK.Agent-${version}-linux-x86_64.AppImage";
-            sha256 = "1gr3q37ldixcqbwpxchhldlfjf7wcygxvnv6ff9nl7l8gxm732l6";
-            #sha256 = "sha256-zbQyouXC8qebYeYbyyl7fulYe4rRens5j+LvW7y8bqI=";
-          };
-          buildCommand = ''
-            mkdir -p $out
-            cp $src $out/appimage
-            chmod ugo+rx $out/appimage
-          '';
-        };
-      in self.runCommand "uhk-agent" {} ''
-        mkdir -p $out/bin $out/etc/udev/rules.d 
-        echo "${self.appimage-run}/bin/appimage-run ${image}/appimage" > $out/bin/uhk-agent
-        chmod +x $out/bin/uhk-agent
-        cat > $out/etc/udev/rules.d/50-uhk60.rules <<EOF
-        # Ultimate Hacking Keyboard rules
-        # These are the udev rules for accessing the USB interfaces of the UHK as non-root users.
-        # Copy this file to /etc/udev/rules.d and physically reconnect the UHK afterwards.
-        SUBSYSTEM=="input", GROUP="input", MODE="0666"
-        SUBSYSTEMS=="usb", ATTRS{idVendor}=="1d50", ATTRS{idProduct}=="612[0-7]", MODE:="0666", GROUP="plugdev"
-        KERNEL=="hidraw*", ATTRS{idVendor}=="1d50", ATTRS{idProduct}=="612[0-7]", MODE="0666", GROUP="plugdev"
-        EOF
-      '';
   };
 
   linuxPackages = pkgs.linuxPackages_latest;
