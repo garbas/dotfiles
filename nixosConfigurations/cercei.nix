b29481410055fcee0f8ae4d7a1281e41908a574a
Rok Garbas <rok@garbas.si>
2023-01-30 01:08:28 +0100

    setting up flox binary cache

--- nixosConfigurations/cercei.nix
+++ nixosConfigurations/cercei.nix
@@ -87,5 +87,9 @@ inputs:
   boot.loader.efi.canTouchEfiVariables = true;
 
   services.spice-vdagentd.enable = true;
-}
 
+  nix.settings.extra-trusted-substituters = [ "https://cache.floxdev.com" ];
+  nix.settings.extra-trusted-public-keys = [ "flox-store-public-0:8c/B+kjIaQ+BloCmNkRUKwaVPFWkriSAd0JJvuDu4F0=" ];
+
+  networking.firewall.allowedTCPPorts = [ 8000 ];
+}
