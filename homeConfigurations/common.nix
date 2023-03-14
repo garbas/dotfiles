d9c1725e82c27bb4f341ec1d45e0c65fedae5bce
Rok Garbas <rok@floxdev.com>
2023-03-14 22:27:14 +0100

    enable zoxide

--- homeConfigurations/common.nix
+++ homeConfigurations/common.nix
@@ -623,8 +623,8 @@ in {
 
   '';
 
-  #programs.zoxide.enable = true;
-  #programs.zoxide.enableZshIntegration = true;
+  programs.zoxide.enable = true;
+  programs.zoxide.enableZshIntegration = true;
 
   programs.zsh.enable = true;
   programs.zsh.enableAutosuggestions = true;
@@ -633,14 +633,6 @@ in {
   programs.zsh.autocd = true;
   programs.zsh.defaultKeymap = "viins";
   programs.zsh.history.expireDuplicatesFirst = true;
-  #programs.zsh.initExtraBeforeCompInit = ''
-  #  # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
-  #  # Initialization code that may require console input (password prompts, [y/n]
-  #  # confirmations, etc.) must go above this block; everything else may go below.
-  #  if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
-  #    source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
-  #  fi
-  #'';
   programs.zsh.plugins = [
     {
       file = "powerlevel10k.zsh-theme";
