2759ce924c4b8d802e3667238e31bcd7aaa2134e
Rok Garbas <rok@garbas.si>
2025-11-04 23:17:33 +0000

    fix

--- homeConfigurations/indigo.nix
+++ homeConfigurations/indigo.nix
@@ -10,7 +10,67 @@
     inputs.flox.homeModules.flox
   ];
 
+  # XXX: due to some problems in flox homeModule
+  nix.settings.substituters = [
+    "https://cache.nixos.org"
+    "https://cache.flox.dev"
+    "https://devenv.cachix.org"
+  ];
+  nix.settings.trusted-public-keys = [
+    "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+    "flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs="
+    "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+  ];
+
   nix.package = pkgs.nixVersions.latest;
+  nix.distributedBuilds = true;
+  nix.buildMachines = [
+    {
+      protocol = "ssh-ng";
+      sshUser = "nixbld";
+      hostName = "hetzner-x86-64-indigo-04";
+      systems = [ "x86_64-linux" ];
+      #sshKey = "/Users/${user.username}/.ssh/id_ed25519";
+      maxJobs = 8;
+      speedFactor = 1;
+      supportedFeatures = [
+        "kvm"
+        "benchmark"
+        "big-parallel"
+        "nixos-test"
+        "docker"
+      ];
+      mandatoryFeatures = [ ];
+      publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSVB0NlA4eGdlMkhpYS84WHRaN2FqNU42Q2FvZWdmQjIwQ252SnlRTG93Q2kgcm9vdEBoZXR6bmVyLXg4Ni02NC1pbmRpZ28tMDQK";
+    }
+    {
+      protocol = "ssh-ng";
+      sshUser = "nixbld";
+      hostName = "hetzner-x86-64-indigo-05";
+      systems = [ "x86_64-linux" ];
+      #sshKey = "/Users/${user.username}/.ssh/id_ed25519";
+      maxJobs = 8;
+      speedFactor = 1;
+      supportedFeatures = [
+        "kvm"
+        "benchmark"
+        "big-parallel"
+        "nixos-test"
+        "docker"
+      ];
+      mandatoryFeatures = [ ];
+      publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSVBYNzc4Qkl1VXlTNHdDd1JOM0hOQU5NeWowZEp3d1lXYzlGTlNFaTJIZW0gcm9vdEBoZXR6bmVyLXg4Ni02NC1pbmRpZ28tMDUK";
+    }
+  ];
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
+  #extra-trusted-public-keys = floxhub-1:0QOAlcobcEvq1mqEf4qAYCaWnTTOXpyoRv/PmqfSixM=
+
+  nixpkgs.config.allowBroken = false;
+  nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.allowUnfreeRedistributable = true;
+
   programs.flox.enable = true;
 
   programs.zsh.initContent = ''
--- homeConfigurations/profiles/common.nix
+++ homeConfigurations/profiles/common.nix
@@ -9,19 +9,6 @@
     (import ./common_neovim.nix)
   ];
 
-  nix.settings.substituters = [
-    "https://cache.nixos.org"
-    "https://cache.flox.dev"
-    "https://devenv.cachix.org"
-  ];
-  nix.settings.trusted-public-keys = [
-    "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-    "flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs="
-    "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
-  ];
-
-  nixpkgs.config.allowUnfree = true;
-
   home.username = user.username;
   home.stateVersion = "22.11";
 
@@ -142,7 +129,8 @@
   programs.git.settings.alias.l = "log --graph --oneline --decorate --all";
   programs.git.settings.alias.b = "branch";
   # list branches sorted by last modified
-  programs.git.settings.alias.bb = "!git for-each-ref --sort='-authordate' --format='%(authordate)%09%(objectname:short)%09%(refname)' refs/heads | sed -e 's-refs/heads/--'";
+  programs.git.settings.alias.bb =
+    "!git for-each-ref --sort='-authordate' --format='%(authordate)%09%(objectname:short)%09%(refname)' refs/heads | sed -e 's-refs/heads/--'";
   programs.git.settings.alias.entr = "!git ls-files -cdmo --exclude-standard | entr -d";
   programs.git.lfs.enable = true;
   programs.git.settings.gpg.format = "ssh";
